097dc679143793209f06ff965db8e067c805a15d
CodeHarrington <christopherharrington79@gmail.com>
2022-03-16 17:21:17 +0000

    add style to css for messages , player and computer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
-let result
+/**
+ * * Declare constants for DOM elements
+ * and possible choices
+ * */
+
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score")
+
+
+
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   userChoice = e.target.id
