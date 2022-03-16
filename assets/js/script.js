dddecf12dc530d5b14f4f2182f735a04aae38267
CodeHarrington <christopherharrington79@gmail.com>
2022-03-16 18:53:51 +0000

    add js scores and win and loses

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,86 +3,131 @@
  * and possible choices
  * */
 
-const buttons = document.getElementsByClassName("control");
-const playerScore = document.getElementById("player-score");
-const computerScore = document.getElementById("computer-score");
-const playerImage = document.getElementById("player-image");
-const computerImage = document.getElementById("computer-image");
-const messages = document.getElementById("messages");
-const choices = ["rock", "paper", "scissors"];
+ const buttons = document.getElementsByClassName("control");
+ const playerScore = document.getElementById("player-score");
+ const computerScore = document.getElementById("computer-score");
+ const playerImage = document.getElementById("player-image");
+ const computerImage = document.getElementById("computer-image");
+ const messages = document.getElementById("messages");
+ const choices = ["rock", "paper", "scissors"];
+ 
+ /**
+  * add event listener to all buttons
+  */
+ 
+ for (let button of buttons) {
+   button.addEventListener("click", function () {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+   });
+ }
+ 
+ /**
+  * The Main game function. accepts parameter which 
+  * is data choice of the selected button 
+  */
+ 
+ function playGame(playerChoice) {
+ 
+   playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+   playerImage.alt = choices[playerChoice];
+ }
+ 
+ let computerChoice = Math.floor(Math.random() * 3 );
+ 
+ computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+ computerImage.alt = choices[computerChoice];
+ 
+ let result = checkWinner(choices[computerChoice], choices[playerChoices]);
+ 
+ updateScore(result);
+ 
+ /**
+  * end 
+  */
+ 
+ //Result to tell you who has won.
 
-/**
- * add event listener to all buttons
- */
-
-for (let button of buttons) {
-  button.addEventListener("click", function () {
-       let playerChoice = this.getAttribute("data-choice");
-       playGame(playerChoice);
-  });
-}
-
-/**
- * The Man game function. accepts parameter which 
- * is data choice of the selected button 
- */
-
-function playGame(playerChoice) {
-
-  playerImage.src = `assets/images/${choices[playerChoice]}.png`;
-  playerImage.alt = choices[playerChoice];
-}
-
-let computerChoice = Math.floor(Math.random() * 3 );
-
-computerImage.src = `assets/images/${choices[computerChoice]}.png`;
-computerImage.alt = choices[computerChoice];
-
-let result = checkWinner(choices[computerChoice], choices[playerChoices]);
-
-updateScore(result);
-
-/**
- * end 
- */
-
- function generateComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
+function getResult(){
+  if(computerChoiceDisplay.innerHTML === 'rock' && userChoice === 'rock'){
+      result = 'Draw, you both have rock!';
+  // Draw
   
-  if (randomNumber === 1) {
-    computerChoice = 'rock'
   }
-  if (randomNumber === 2) {
-    computerChoice = 'scissors'
+  if(computerChoiceDisplay.innerHTML === 'paper' && userChoice === 'paper'){
+      result = 'Draw, you both have paper!';
+  // Draw
+
   }
-  if (randomNumber === 3) {
-    computerChoice = 'paper'
+  if(computerChoiceDisplay.innerHTML === 'scissors' && userChoice === 'scissors'){
+      result = 'Draw, you both have scissors!';
+  // Draw
+
   }
-  computerChoiceDisplay.innerHTML = computerChoice
+ 
+  
+  if(computerChoiceDisplay.innerHTML === 'rock' && userChoice === 'paper'){
+      result = 'You Win, paper wraps rock !';
+      win();
+
+  }
+  if(computerChoiceDisplay.innerHTML === 'rock' && userChoice === 'scissors'){
+      result = 'You lost!, scissors gets jammed by rock  ';
+      lose();
+  
+ 
+  
+  if(computerChoiceDisplay.innerHTML === 'paper' && userChoice === 'scissors'){
+      result= 'You Win!, scissors cuts paper';
+      win();
+      
+  }
+  if(computerChoiceDisplay.innerHTML === 'paper' && userChoice === 'rock'){
+      result= 'You lost!, rock gets wrapped by paper';
+      lose();
+      
+  }
+  
+ 
+  if(computerChoiceDisplay.innerHTML === 'scissors' && userChoice === 'rock'){
+      result= 'You Win!, rock jams scissors';
+      win();
+      
+  }
+  if(computerChoiceDisplay.innerHTML === 'scissors' && userChoice === 'paper'){
+      result= 'You lost!, paper gets cut by scissors';
+      lose();
+      
+  }
+ 
+  resultDisplay.innerHTML = result;
 }
 
-function getResult() {
-    if (computerChoice === userChoice) {
-      result = 'its a draw!'
-    }
-    if (computerChoice === 'rock' && userChoice === "paper") {
-      result = 'you win!'
-    }
-    if (computerChoice === 'rock' && userChoice === "scissors") {
-      result = 'you lost!'
-    }
-    if (computerChoice === 'paper' && userChoice === "scissors") {
-      result = 'you win!'
-    }
-    if (computerChoice === 'paper' && userChoice === "rock") {
-      result = 'you lose!'
-    }
-    if (computerChoice === 'scissors' && userChoice === "rock") {
-      result = 'you win!'
-    }
-    if (computerChoice === 'scissors' && userChoice === "paper") {
-      result = 'you lose!'
-    }
-    resultDisplay.innerHTML = result
-  }
+//Gets current score from DOM and increments by 1
 
+function win(){
+  playerScore++;
+  playerScoreSpan.innerHTML = playerScore;
+  computerScoreSpan.innerHTML = computerScore;
+  
+}
+function lose(){
+  computerScore++;
+  computerScoreSpan.innerHTML = computerScore;
+  playerScoreSpan.innerHTML = playerScore;
+
+}
+// resets the game at 15 and tells whomever gets there first
+function gameOver(){
+  if(playerScore === 15 && computerScore < 15){
+      alert("You won press ok to reset the game");
+      playerScore = 0;
+      computerScore = 0;
+  }
+  if(playerScore < 15 && computerScore === 15){
+      alert("You lost press ok to reset the game");
+      playerScore = 0;
+      computerScore = 0;
+
+  }
+}
